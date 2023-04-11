90f49ed1c16a7412217f8643ebff90df0cc57d27
yeeyungong <51457638+yeeyungong@users.noreply.github.com>
2023-04-11 15:11:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.c8fkypxc9olt.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ongyeeyung-employee"
+customregion = "us-east-1"
